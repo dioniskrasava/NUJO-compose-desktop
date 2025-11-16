8a6361fd69c3e59b103cc1529b253a59cee773cd
dionis.krasava <dionis.krasava@gmail.com>
2025-11-16 12:28:01 +0700

    коммит от 16 ноября 2025 года )

--- src/main/kotlin/app/nujo/Main.kt
+++ src/main/kotlin/app/nujo/Main.kt
@@ -8,6 +8,7 @@ import app.nujo.state.AppState
 import app.nujo.ui.NutritionApp
 import app.nujo.ui.NutritionTrackerTheme
 
+// коммит из 16 ноября 2025 года!
 
 fun main() = application {
     val appState = remember { AppState() }
